4ec5610e600d9db14d91d95d6409e1a4f2b3ecb0
aldlss <ayaldlss@gmail.com>
2022-04-22 23:53:15 +0800

    20220421 un ok

---
+++ 202204/20220422.cpp
@@ -0,0 +1,14 @@
+//
+// Created by aldlss on 2022/4/22.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int sum=accumulate(nums.begin(),nums.end(),0),Rsum=0;
+        nums.insert(nums.end(),nums.begin(),nums.end());
+        for()
+    }
+};
