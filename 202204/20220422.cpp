60239c3de517312c40b4b6ce8d3edc0348eebd24
aldlss <ayaldlss@gmail.com>
2022-04-23 10:32:40 +0800

    20220422

--- 202204/20220422.cpp
+++ 202204/20220422.cpp
@@ -7,8 +7,19 @@ using namespace std;
 class Solution {
 public:
     int maxRotateFunction(vector<int>& nums) {
-        int sum=accumulate(nums.begin(),nums.end(),0),Rsum=0;
-        nums.insert(nums.end(),nums.begin(),nums.end());
-        for()
+//        int sum=accumulate(nums.begin(),nums.end(),0);
+        int Rsum=0,ans=-0x7FFFFFFF,n=nums.size(),sum=0;
+        for(int i=0;i<n;++i)
+        {
+            Rsum+=i*nums[i];
+            sum+=nums[i];//这样写还更慢了呃呃
+        }
+        ans=max(ans,Rsum);
+        for(int i=n-1;i>0;--i)
+        {
+            Rsum+=sum-nums[i]*n;
+            ans=max(ans,Rsum);
+        }
+        return ans;
     }
 };
