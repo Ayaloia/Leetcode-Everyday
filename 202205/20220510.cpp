2ac09455e52e483ee633fd07796295491955192d
aldlss <ayaldlss@gmail.com>
2022-05-10 16:20:06 +0800

    20220510 incomplete

---
+++ 202205/20220510.cpp
@@ -0,0 +1,13 @@
+//
+// Created by aldlss on 2022/5/10.
+//
+
+//1728. 猫和老鼠 II
+//节省人生时间我选择开摆
+
+class Solution {
+public:
+    bool canMouseWin(vector<string>& grid, int catJump, int mouseJump) {
+
+    }
+};
